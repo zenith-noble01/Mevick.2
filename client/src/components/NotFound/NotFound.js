6a06c70bc41bbb75939ff0df616080629a44f55e
zenith-noble01 <louisefongang@gmail.com>
2021-09-02 13:49:13 +0100

    the styling of the notfound page

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import "./notfound.css"
 import { useParams } from "react-router"
+import { Link } from "react-router-dom"
 import logo from '../images/logo.png'
 
 const NotFound = () => {
@@ -8,10 +9,15 @@ const NotFound = () => {
         <div className="notfound">
             <h1>404</h1>
             <div className="notfoundContainer">
-                <img src={logo} alt="" />
+                <img src={logo} alt="" className="notfoundImg"/>
                <p className="mev">Mevick Notfound</p>
             </div>
             <p>{JSON.stringify(params)}</p>
+            <div className="notBack">
+                <Link to="/" className="backTohome">
+                   Back to Home
+                </Link>
+            </div>
         </div>
     )
 }
