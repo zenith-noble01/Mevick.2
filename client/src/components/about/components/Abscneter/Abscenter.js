7f586b55a0352786e4ea9cb3880f54259c4ed386
zenith-noble01 <webdev686@gmail.com>
2021-09-08 01:49:49 +0100

    the other features

--- client/src/components/about/components/Abscneter/Abscenter.js
+++ client/src/components/about/components/Abscneter/Abscenter.js
@@ -1,11 +1,19 @@
-import "./acen.css"
-
-const Center = () => {
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react'
+export class MapContainer extends Component {
+  render() {
     return (
-        <div className="center">
-            am the center
-        </div>
-    )
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Yaounde'} />
+        <InfoWindow onClose={this.onInfoWindowClose}>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
-
-export default Center
+ 
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyDp10SNvbm5xa9HrjP-fP1TDCytl_Oe27I')
+})(MapContainer)
