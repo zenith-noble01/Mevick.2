409b851b747435e89664d3107ac8c33d19951791
zenith-noble01 <webdev686@gmail.com>
2021-09-09 01:47:36 +0100

    please do me this man

--- client/src/components/about/components/Abscneter/Abscenter.js
+++ client/src/components/about/components/Abscneter/Abscenter.js
@@ -1,19 +1,11 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import React, { Component } from 'react'
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Yaounde'} />
-        <InfoWindow onClose={this.onInfoWindowClose}>
-        </InfoWindow>
-      </Map>
-    );
-  }
+import "./abcenter.css"
+
+const Abscenter = () => {
+  return (
+    <div className="abscenter">
+      am the center
+    </div>
+  )
 }
- 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyDp10SNvbm5xa9HrjP-fP1TDCytl_Oe27I')
-})(MapContainer)
+
+export default Abscenter
--- client/src/components/about/components/aboutSidebar/AbSidebar.js
+++ client/src/components/about/components/aboutSidebar/AbSidebar.js
@@ -1,9 +1,21 @@
 import "./abssidebar.css"
+import logo from '../../../images/logo.png'
+import { Link } from 'react-router-dom'
+
 
 const AbSidebar = () => {
     return (
         <div className="abssidebar">
- it
+            <div className="absidebarTop">
+                <span>About</span>
+                <div className="div1">
+                    <Link to="/" className="div1">
+                    <img src={logo} className="
+                    absidebarImg"/>
+                    <span className="abssidebarTopTitle">MEVICK</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
