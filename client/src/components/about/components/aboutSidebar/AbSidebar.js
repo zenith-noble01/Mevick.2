85572cf8b6229eb25d7814a3b57e062392a5a917
zenith-noble01 <webdev686@gmail.com>
2021-09-11 01:38:28 +0100

    it worked

--- client/src/components/about/components/aboutSidebar/AbSidebar.js
+++ client/src/components/about/components/aboutSidebar/AbSidebar.js
@@ -1,9 +1,12 @@
 import "./abssidebar.css"
 import logo from '../../../images/logo.png'
 import { Link } from 'react-router-dom'
-
+import { useState } from "react"
 
 const AbSidebar = () => {
+
+    // const [sidebar, setSidebar] = useState(false)
+    const [sidebarDropdown, setSidebarDropdown] = useState(false)
     return (
         <div className="abssidebar">
             <div className="absidebarTop">
@@ -19,6 +22,19 @@ const AbSidebar = () => {
             <div className="sidebarInput">
                 <i className="searchIcon fa fa-search"></i>
                 <input type="text" placeholder="Search" />
+            </div>
+            <div className="sidebarWrapper">
+                <ul className="sidebarItem">
+                    <li className="sidebarItemList1" onClick={() => setSidebarDropdown(!sidebarDropdown)}> Institution <i className={sidebarDropdown? 'chev fas fa-chevron-down' : "chev fas fa-chevron-up"}></i></li>
+                    <ul className={sidebarDropdown ? "drop" : "dropdownList"}>
+                        <li className="dropdownItem"><i className="sidbarDropIcon fas fa-book-open"></i>Library</li>
+                        <li className="dropdownItem"><i className="sidbarDropIcon fas fa-flask"></i>Labs</li>
+                        <li className="dropdownItem"><i className="sidbarDropIcon fas fa-shield-alt"></i>Fields</li>
+                        <li className="dropdownItem"><i className="sidbarDropIcon fas fa-cookie-bite"></i>Cantine</li>
+                        <li className="dropdownItem"><i className="sidbarDropIcon fas fa-binoculars"></i>Overview</li>
+                    </ul>
+                    <li className="sidebarItemList">zenith</li>
+                </ul>
             </div>
         </div>
     )
