bdd09044bce6b968f0f8994c112c3a43834ddebf
zenith-noble01 <webdev686@gmail.com>
2021-09-30 01:44:03 +0100

    zenith added the other features

---
+++ client/src/components/admin/components/chatcontent/ChatContent.js
@@ -0,0 +1,18 @@
+import "./chatcontent.css"
+
+const ChatContent = ({message, setCurrentMessage}) => {
+    // console.log(message);
+    return (
+        <div className="chatContent">
+            <i className="chatContentIcon fa fa-arrow-left" onClick ={() => setCurrentMessage(null)}></i>
+            <div className="Chatitems">
+                <p className="ContentName">{message.name}</p>
+                <p className="ContentEmail">{message.email}</p>
+                <p className="ContentPhone">{message.phone}</p>
+                <p className="ContentMessage">{message.message}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ChatContent
