2cd47606482a22fb2701f12bbb39542d983b2f9e
zenith-noble01 <webdev686@gmail.com>
2021-10-06 06:56:03 +0100

    some downloads were made

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ const path = require("path");
 dotenv.config();
 app.use(cors())
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 7000
 
 // const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Mevick',
+mongoose.connect("mongodb+srv://zenith:zenith@miles.f8mrs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
  {useNewUrlParser: true,
   useUnifiedTopology: true}).then(console.log('connected'));
 
